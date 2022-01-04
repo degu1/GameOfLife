433f18bfb2809a6a1789fa812acc024d2872012d
Dennis Gustafsson <dennisgust@gmail.com>
2022-01-04 12:37:59 +0100

    Test aliveCellFourNeighboursDiesWhenRender and aliveCellOneNeighboursDiesWhenRender

--- src/test/java/gameOfLife/GameOfLifeTest.java
+++ src/test/java/gameOfLife/GameOfLifeTest.java
@@ -61,4 +61,35 @@ class GameOfLifeTest {
 
         assertThat(gameOfLife.getCell(new Position(1, 1))).isEqualTo(expected);
     }
+
+    @Test
+    void aliveCellOneNeighboursDiesWhenRender() {
+        GameOfLife gameOfLife = new GameOfLife.GameOfLifeBuilder(3, 3)
+                .cellAlive(new Position(1, 1))
+                .cellAlive(new Position(0, 1))
+                .build();
+
+        gameOfLife.render();
+
+        Cell expected = new Cell(new Position(1, 1));
+
+        assertThat(gameOfLife.getCell(new Position(1, 1))).isEqualTo(expected);
+    }
+
+    @Test
+    void aliveCellFourNeighboursDiesWhenRender() {
+        GameOfLife gameOfLife = new GameOfLife.GameOfLifeBuilder(3, 3)
+                .cellAlive(new Position(1, 1))
+                .cellAlive(new Position(0, 1))
+                .cellAlive(new Position(0, 2))
+                .cellAlive(new Position(1, 0))
+                .cellAlive(new Position(1, 2))
+                .build();
+
+        gameOfLife.render();
+
+        Cell expected = new Cell(new Position(1, 1));
+
+        assertThat(gameOfLife.getCell(new Position(1, 1))).isEqualTo(expected);
+    }
 }
